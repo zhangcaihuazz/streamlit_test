f859ff95926b98d28800c67cb1aec516e3278807
zhangcaihuazz <49941679+zhangcaihuazz@users.noreply.github.com>
2025-11-19 20:18:27 +0800

    streamlit_test

--- streamlit_test_0.1.py
+++ streamlit_test_0.1.py
@@ -1,11 +1,1 @@
-import streamlit as st
-
-# 最简化的页面配置，不使用任何自定义CSS
-st.set_page_config(
-    page_title="极简测试",
-    page_icon="165",
-    layout="centered"
-)
-
-st.title("极简测试页面")
-st.write("这个页面只使用最基本的Streamlit组件")
+print('hello')
